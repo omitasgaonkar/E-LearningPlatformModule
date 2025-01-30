4e3e63d31ba9ce0cc94add4c34f1ca30983d47ba
omkar tasgaonkar <omkartasgaonkar25@gmail.com>
2025-01-30 20:38:55 +0530

    update Address field

--- src/main/java/E_LearningPlatformModule/entity/Emp.java
+++ src/main/java/E_LearningPlatformModule/entity/Emp.java
@@ -7,6 +7,26 @@ public class Emp {
 	private String name;
 
 	private String city;
+	
+	private String address;
+	
+	
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 	public Integer getId() {
 		return id;
