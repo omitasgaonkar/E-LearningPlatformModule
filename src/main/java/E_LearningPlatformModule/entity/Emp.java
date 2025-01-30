ec5ca882a125d89ddc397b967fa0e18242669c20
omkar tasgaonkar <omkartasgaonkar25@gmail.com>
2025-01-30 20:21:23 +0530

    employee class added

---
+++ src/main/java/E_LearningPlatformModule/entity/Emp.java
@@ -0,0 +1,27 @@
+package E_LearningPlatformModule.entity;
+
+public class Emp {
+	
+	private Integer id;
+	
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
